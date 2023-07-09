79f5e58362d9e320320a5b408dbe670927f9a072
Suyash8 <77190293+Suyash8@users.noreply.github.com>
2023-07-09 15:13:31 +0000

    implemented conversion functions for int, std::string, and float data types

--- utils.h
+++ utils.h
@@ -3,80 +3,77 @@
 #include <sstream>
 #include <string>
 
-template <typename T>
-T abs(T n) {
-    return (n < 0) ? -n : n;
-}
+template <typename T> T abs(T n) { return (n < 0) ? -n : n; }
 
-std::string ascii(const std::string& input) {
-    std::string result;
+std::string ascii(const std::string &input) {
+  std::string result;
 
-    for (char c : input) {
-        if (static_cast<unsigned char>(c) > 127) {
-            // Replace non-ASCII character with escape sequence
-            result += "\\x" + std::to_string(static_cast<unsigned char>(c));
-        } else {
-            result += c;
-        }
+  for (char c : input) {
+    if (static_cast<unsigned char>(c) > 127) {
+      // Replace non-ASCII character with escape sequence
+      result += "\\x" + std::to_string(static_cast<unsigned char>(c));
+    } else {
+      result += c;
     }
+  }
 
-    return result;
+  return result;
 }
 
 template <typename T>
-T input(const std::string& inputString, bool safe = false) {
-    T input;
-    if (safe) {
-        bool isValidInput = false;
-        
-        while (!isValidInput) {
-            std::cout << inputString;
-            std::cin >> input;
+T input(const std::string &inputString, bool safe = false) {
+  T input;
+  if (safe) {
+    bool isValidInput = false;
 
-					
-            if (std::cin.fail()) {
-                std::cout << "Invalid input. Please try again." << "\n";
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            } else {
-                isValidInput = true;
-            }
-        }
-    } else {
-        std::cout << inputString;
-        std::cin >> input;
+    while (!isValidInput) {
+      std::cout << inputString;
+      std::cin >> input;
+
+      if (std::cin.fail()) {
+        std::cout << "Invalid input. Please try again."
+                  << "\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      } else {
+        isValidInput = true;
+      }
     }
+  } else {
+    std::cout << inputString;
+    std::cin >> input;
+  }
 
-    return input;
+  return input;
 }
 
-std::string input(const std::string& inputString, bool safe = false) {
-    std::string input;
-    if (safe) {
-        bool isValidInput = false;
-        
-        while (!isValidInput) {
-            std::cout << inputString;
-            std::getline(std::cin >> std::ws, input);
+std::string input(const std::string &inputString, bool safe = false) {
+  std::string input;
+  if (safe) {
+    bool isValidInput = false;
 
-            if (std::cin.fail()) {
-                std::cout << "Invalid input. Please try again." << "\n";
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            } else {
-                isValidInput = true;
-            }
-        }
-    } else {
-        std::cout << inputString;
-        std::cin >> input;
+    while (!isValidInput) {
+      std::cout << inputString;
+      std::getline(std::cin >> std::ws, input);
+
+      if (std::cin.fail()) {
+        std::cout << "Invalid input. Please try again."
+                  << "\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      } else {
+        isValidInput = true;
+      }
     }
+  } else {
+    std::cout << inputString;
+    std::cin >> input;
+  }
 
-    return input;
+  return input;
 }
 
-template <typename T> 
-std::string to_string(const T &obj) {
+template <typename T> std::string to_string(const T &obj) {
   std::ostringstream oss;
   oss << obj;
   return oss.str();
@@ -94,10 +91,10 @@ public:
     return *this;
   }
 
-	PrintOptions &flush(const bool &f = true) {
-		flush_ = f;
-		return *this;
-	}
+  PrintOptions &flush(const bool &f = true) {
+    flush_ = f;
+    return *this;
+  }
 
   std::string separator() const { return separator_; }
   std::string end() const { return end_; }
@@ -105,11 +102,10 @@ public:
 private:
   std::string separator_ = " ";
   std::string end_ = "\n";
-	bool flush_ = false;
+  bool flush_ = false;
 };
 
-template <typename... Args> 
-void print(const Args &...args) {
+template <typename... Args> void print(const Args &...args) {
   std::ostringstream oss;
   ((oss << to_string(args) << PrintOptions().separator()), ...);
   // Add the end string to the output
@@ -124,4 +120,70 @@ void print(const PrintOptions &options, const Args &...args) {
   // Add the end string to the output
   oss << options.end();
   std::cout << oss.str();
+}
+
+long long stringToInt(const std::string &str) {
+  long long result = 0;
+  bool isNegative = false;
+  size_t startIndex = 0;
+
+  // Check if the string is empty
+  if (str.empty()) {
+    // Handle the error case appropriately
+    throw std::invalid_argument(
+        "Empty string cannot be converted to an integer.");
+  }
+
+  // Check if the string represents a negative number
+  if (str[0] == '-') {
+    isNegative = true;
+    startIndex = 1;
+  }
+
+  // Convert each digit character to its corresponding integer value
+  for (size_t i = startIndex; i < str.length(); i++) {
+    if (str[i] >= '0' && str[i] <= '9') {
+      long long digitValue = str[i] - '0'; // Convert character to integer value
+
+      // Multiply the result by 10 and add the digit value
+      result = result * 10 + digitValue;
+    } else {
+      // Handle the error case for invalid characters in the string
+      throw std::invalid_argument("Invalid character found in the string.");
+    }
+  }
+
+  // Apply the negative sign if necessary
+  if (isNegative) {
+    result = -result;
+  }
+
+  return result;
+}
+
+std::string intToString(long long number) {
+    std::ostringstream oss;
+    oss << number;
+    return oss.str();
+}
+
+long long floatToInt(long double number) {
+    return static_cast<long long>(number);
+}
+
+std::string floatToString(long double number) {
+    std::ostringstream oss;
+    oss << number;
+    return oss.str();
+}
+
+long double intToFloat(long long number) {
+    return static_cast<long double>(number);
+}
+
+long double stringToFloat(const std::string& str) {
+    std::istringstream iss(str);
+    long double number;
+    iss >> number;
+    return number;
 }
